d0e8a6dbf516d3f05d1d6ab68091bae06f1a2c3b
chad <chad@chad.tw>
2016-12-20 01:27:59 +0800

    add get Class List

--- courseQuest.py
+++ courseQuest.py
@@ -2,10 +2,13 @@
 import json
 import requests
 from BeautifulSoup import BeautifulSoup as bs4
-dicts = []
 
 # initial request paraments
 s = requests.session()
+header = {'Content-Type': 'application/json; charset=UTF-8'}
+degrees = ['??','大學部','??','碩士班','博士班','進修學士班']
+departments = {}
+units = {}
 
 def init():
     url = "http://service002.sds.fcu.edu.tw/main.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODIwODY2MTN9.zRhVTM5MHdVFuZFbvFW4gwrm3sTg_33EyTZECZ22DrM"
@@ -13,41 +16,54 @@ def init():
 
 # get Department Lists
 def getDeptList():
-    global dicts
+    #global departments,header,degree
     open("DepartmentList.txt","w").write('')
-    degree = ['??','大學部','??','碩士班','博士班','進修學士班']
     url = "http://service002.sds.fcu.edu.tw/Service/Search.asmx/GetDeptList"
-    header = {
-        'Content-Type': 'application/json; charset=UTF-8'
-    }
     # Six Degree
-    for i in range(6):
-        data = {
-            "baseOptions":{"lang":"cht","year":105,"sms":2},"degree":i
-        }    
+    for degree in range(6):
+        data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree}    
         r = s.post(url,headers=header,json=data);
-        content = "// " + degree[i] + "\n" + r.text.encode('utf-8') + '\n'
+        content = "// " + degrees[degree] + "\n" + r.text.encode('utf-8') + '\n'
         open("DepartmentList.txt","a").write(content)
-        dicts.append(json.loads(r.text))
+        departments[degree] = json.loads(r.text)
+        print degrees[degree] + " crawled"
     
 # get Department Lists
 def getUnitList():
-    pass
+    #global departments,degree,header
+    open("UnitList.txt","w").write('')
+    url = "http://service002.sds.fcu.edu.tw/Service/Search.asmx/GetUnitList"
+    # Six Degree
+    for degree in departments:
+        depts = json.loads(departments[degree]['d'])
+        units[degree] = depts 
+        j = 0
+        for dept in depts:
+            print dept['name'].encode('utf-8') + " crawed"
+            data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree,"deptId":dept['id']}
+            r = s.post(url,headers=header,json=data);
+            content = "// " + degrees[degree] + " " + dept['name'].encode('utf-8') + "\n" + r.text.encode('utf-8') + '\n'
+            open("UnitList.txt","a").write(content)
+            units[degree][j]['unit'] = json.loads(json.loads(r.text)['d'])
+            j = j+1
+
+# get Department Lists
+def getClassList():
+    #global dicts,degree,header
+    open("ClassList.txt","w").write('')
+    url = "http://service004.sds.fcu.edu.tw/Service/Search.asmx/GetClassList"
+    # Six Degree
+    for degree in units:
+        for depts in units[degree]:
+            for unit in depts['unit']:
+                print unit['name'].encode('utf-8') + " crawed"
+                data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree,"unitId":unit['id']}
+                r = s.post(url,headers=header,json=data);
+                content = "// " + degrees[degree] + " " + depts['name'].encode('utf-8') + " " + unit['name'].encode('utf-8') + "\n" + r.text.encode('utf-8') + "\n"
+                open("ClassList.txt","a").write(content)
+
 init()
 getDeptList()
-
-open("UnitList.txt","w").write('')
-degree = ['??','大學部','??','碩士班','博士班','進修學士班']
-url = "http://service002.sds.fcu.edu.tw/Service/Search.asmx/GetUnitList"
-header = {
-    'Content-Type': 'application/json; charset=UTF-8'
-}
-# Six Degree
-#for i in range(6):
-for dept in range(5):
-    data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":"1","deptId":dept}
-    r = s.post(url,headers=header,json=data);
-    content = "// " + degree[dept] + "\n" + r.text.encode('utf-8') + '\n'
-    open("DepartmentList.txt","a").write(content)
+getUnitList()
+getClassList()
     
-
