bbee6801cd79f69a1b3d43cb0531ef36b85500d6
chad <chad@chad.tw>
2016-12-20 14:54:21 +0800

    add output dirs

--- courseQuest.py
+++ courseQuest.py
@@ -9,6 +9,7 @@ header = {'Content-Type': 'application/json; charset=UTF-8'}
 degrees = ['??','大學部','??','碩士班','博士班','進修學士班']
 departments = {}
 units = {}
+classs = {}
 
 def init():
     url = "http://service002.sds.fcu.edu.tw/main.aspx?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODIwODY2MTN9.zRhVTM5MHdVFuZFbvFW4gwrm3sTg_33EyTZECZ22DrM"
@@ -17,21 +18,20 @@ def init():
 # get Department Lists
 def getDeptList():
     #global departments,header,degree
-    open("DepartmentList.txt","w").write('')
+    open("output/DepartmentList.txt","w").write('')
     url = "http://service002.sds.fcu.edu.tw/Service/Search.asmx/GetDeptList"
     # Six Degree
     for degree in range(6):
         data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree}    
         r = s.post(url,headers=header,json=data);
         content = "// " + degrees[degree] + "\n" + r.text.encode('utf-8') + '\n'
-        open("DepartmentList.txt","a").write(content)
+        open("output/DepartmentList.txt","a").write(content)
         departments[degree] = json.loads(r.text)
         print degrees[degree] + " crawled"
     
 # get Department Lists
 def getUnitList():
-    #global departments,degree,header
-    open("UnitList.txt","w").write('')
+    open("output/UnitList.txt","w").write('')
     url = "http://service002.sds.fcu.edu.tw/Service/Search.asmx/GetUnitList"
     # Six Degree
     for degree in departments:
@@ -43,27 +43,32 @@ def getUnitList():
             data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree,"deptId":dept['id']}
             r = s.post(url,headers=header,json=data);
             content = "// " + degrees[degree] + " " + dept['name'].encode('utf-8') + "\n" + r.text.encode('utf-8') + '\n'
-            open("UnitList.txt","a").write(content)
+            open("output/UnitList.txt","a").write(content)
             units[degree][j]['unit'] = json.loads(json.loads(r.text)['d'])
             j = j+1
 
 # get Department Lists
 def getClassList():
-    #global dicts,degree,header
-    open("ClassList.txt","w").write('')
+    open("output/ClassList.txt","w").write('')
     url = "http://service004.sds.fcu.edu.tw/Service/Search.asmx/GetClassList"
     # Six Degree
     for degree in units:
+        # classs[degree] = depts 
+        j = 0
         for depts in units[degree]:
+            # classs[degree][j]['unit'] = unit
+            j = j+1
             for unit in depts['unit']:
-                print unit['name'].encode('utf-8') + " crawed"
                 data = {"baseOptions":{"lang":"cht","year":105,"sms":2},"degree":degree,"unitId":unit['id']}
                 r = s.post(url,headers=header,json=data);
+                print unit['name'].encode('utf-8') + " crawed"
                 content = "// " + degrees[degree] + " " + depts['name'].encode('utf-8') + " " + unit['name'].encode('utf-8') + "\n" + r.text.encode('utf-8') + "\n"
-                open("ClassList.txt","a").write(content)
+                open("output/ClassList.txt","a").write(content)
+    # return classs
 
 init()
 getDeptList()
 getUnitList()
 getClassList()
+#getType1Result()
     
